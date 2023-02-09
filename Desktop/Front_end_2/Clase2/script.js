20cddcfabaeaf5c92153765b6ff6853f31ec1878
Juanmapp <juanma1694@gmail.com>
2023-02-09 13:04:10 -0300

    Primer commit

---
+++ Desktop/Front_end_2/Clase2/script.js
@@ -0,0 +1,12 @@
+//alert
+
+ alert("Esto es un alerta!")
+
+//confirm
+let confirmacion = confirm("Estas seguro de aceptar?")
+
+console.log(confirmacion)
+
+//prompt
+
+console.log(prompt("Ingresa tu nombre completo"))
