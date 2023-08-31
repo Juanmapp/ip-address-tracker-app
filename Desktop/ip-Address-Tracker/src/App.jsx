e106f805628ba53e0480e75ad8ca090b5ef58f7b
Juanmapp <juanma1694@gmail.com>
2023-08-31 18:15:25 -0300

    Agregamos el mapa

--- Desktop/ip-Address-Tracker/src/App.jsx
+++ Desktop/ip-Address-Tracker/src/App.jsx
@@ -7,13 +7,6 @@ function App() {
       {/* Aca va nuestro codigo */}
       <Header />
       {/* Derechos de autor */}
-      <div>
-        Challenge by{" "}
-        <a href="https://www.frontendmentor.io?ref=challenge">
-          Frontend Mentor
-        </a>
-        . Coded by <a href="#">Your Name Here</a>.
-      </div>
     </>
   );
 }
